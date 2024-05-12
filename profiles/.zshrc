dedd9af1840e094e83a48ac096e12b6bdffbbd28
crzorz <crzorz@outlook.com>
2024-05-12 17:21:38 +0800

    增加mac的环境文件

---
+++ profiles/.zshrc
@@ -0,0 +1,26 @@
+## Themes
+
+ZSH_THEME="ys"
+
+
+## Plugins
+
+plugins=(
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+
+## oh-my-zsh
+
+### 关闭oh-my-zsh自动更新
+export DISABLE_AUTO_UPDATE=true
+
+export MY_ZSH=$MELON_ROOT/profiles/oh-my-zsh
+source $MY_ZSH/oh-my-zsh.sh
+
+
+## Source
+
+[ -f ~/.bash_profile ] && source ~/.bash_profile
---
+++ scripts/logger.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Created by crzorz on 2022/09/16
+# Copyright © 2022 BaldStudio. All rights reserved.
+
+# debug:0; info:1; warn:2; error:3
+log_level=0
+log_detail=0
+
+logging() {
+  local log_type
+  log_type=$1
+  readonly log_type
+
+  local msg
+  msg=$2
+  readonly msg
+
+  local now
+  now=$(date +'%F %H:%M:%S')
+  readonly now
+
+  local log_format="[${now}][${log_type}] ${msg}"
+  if [[ ${log_detail} -eq 1 ]]; then
+    log_format="[${now}] [${log_type}] [${FUNCNAME[2]} - $(caller 0 | awk '{print$1}')] ${msg}"
+  fi
+
+  case $log_type in
+  DEBUG)
+    if [[ $log_level -le 0 ]]; then
+      echo -e "\033[37m${log_format}\033[0m"
+    fi
+    ;;
+  INFO)
+    if [[ $log_level -le 1 ]]; then
+      echo -e "\033[32m${log_format}\033[0m"
+    fi
+    ;;
+  WARNING)
+    if [[ $log_level -le 2 ]]; then
+      echo -e "\033[33m${log_format}\033[0m"
+    fi
+    ;;
+  ERROR)
+    if [[ $log_level -le 3 ]]; then
+      echo -e "\033[31m${log_format}\033[0m"
+    fi
+    ;;
+  esac
+}
+
+log_debug() {
+  logging DEBUG "$*"
+}
+
+log_info() {
+  logging INFO "$*"
+}
+
+log_warning() {
+  logging WARNING "$*"
+}
+
+log_error() {
+  logging ERROR "$*"
+}
