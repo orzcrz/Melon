9a60069d6d823c90576e5f27655052d970b47a43
crzorz <crzorz@outlook.com>
2024-08-21 10:46:46 +0800

    🌼 增加workspace工具

--- melon/subcommands/__init__.py
+++ melon/subcommands/__init__.py
@@ -5,8 +5,10 @@ Copyright © 2024 BaldStudio. All rights reserved.
 
 from .release import Release
 from .config import Config
+from .workspace import Workspace
 
 __subcommands__ = [
     Release,
     Config,
+    Workspace,
 ]
---
+++ melon/subcommands/workspace/__init__.py
@@ -0,0 +1,7 @@
+"""
+Created by crzorz on 2024/08/20
+Copyright © 2024 SHEIN. All rights reserved.
+"""
+
+# 基于 git worktree 的常用命令集
+from .workspace import Workspace
